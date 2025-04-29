738524bf04374451040f0dc41042e3393bccfaf0
Joeunseon <wincr_es@naver.com>
2025-04-30 08:45:57 +0900

    feat: add user info page

--- src/main/java/com/project/demo/web/user/UserController.java
+++ src/main/java/com/project/demo/web/user/UserController.java
@@ -3,6 +3,7 @@ package com.project.demo.web.user;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
@@ -11,5 +12,13 @@ public class UserController {
     public String list(ModelMap model) {
 
         return "/user/list";
+    }
+
+    @GetMapping("/user/info")
+    public String info(@RequestParam(value = "userSeq", required = false) Long userSeq, ModelMap model) {
+
+        model.addAttribute("userSeq", userSeq);
+
+        return "/user/info";
     }
 }
